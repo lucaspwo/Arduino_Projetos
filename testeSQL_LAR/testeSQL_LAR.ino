4f8f28051cff2d64ee9c06aa1af20b1a4d2d1f02
Lucas Oliveira <lucaspwo@gmail.com>
2016-04-15 18:44:27 -0300

    Código SQL
    Teste de conexão com db SQL no LAR

---
+++ testeSQL_LAR/testeSQL_LAR.ino
@@ -0,0 +1,35 @@
+/**
+* Example: Hello, MySQL!
+*
+* This code module demonstrates how to create a simple
+* database-enabled sketch.
+*/
+//#include "SPI.h"
+//#include "Ethernet.h"
+#include "sha1.h"
+#include "mysql.h"
+
+/* Setup for Ethernet Library */
+byte mac_addr[] = { 0x90, 0xA2, 0xDA, 0x00, 0xC9, 0xB9 };
+IPAddress server_addr(10, 0, 0, 113);
+
+/* Setup for the Connector/Arduino */
+Connector my_conn; // The Connector/Arduino reference
+char user[] = "arduino";
+char password[] = "senha";
+
+void setup() {
+ Ethernet.begin(mac_addr);
+ Serial.begin(9600);
+ while (!Serial);
+ delay(1000);
+ Serial.println("Connecting...");
+ if (my_conn.mysql_connect(server_addr, 3306, user, password)){
+ Serial.println("Success!");
+ } else {
+ Serial.println("Connection failed.");
+ }
+}
+
+void loop() {
+}

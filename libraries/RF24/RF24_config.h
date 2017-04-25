237c8cc764f4c125b87b725c3bb06274365c391a
Lucas Oliveira <lucaspwo@gmail.com>
2017-04-24 23:25:44 -0300

    Atual. bilbiotecas

--- libraries/RF24/RF24_config.h
+++ libraries/RF24/RF24_config.h
@@ -5,8 +5,6 @@
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
  version 2 as published by the Free Software Foundation.
-
- Added Arduino Due support from https://github.com/mcrosson/
  */
  
  /* spaniakos <spaniakos@gmail.com>
@@ -22,53 +20,51 @@
   //#define MINIMAL
   //#define SPI_UART  // Requires library from https://github.com/TMRh20/Sketches/tree/master/SPI_UART
   //#define SOFTSPI   // Requires library from https://github.com/greiman/DigitalIO
+  
   /**********************/
   #define rf24_max(a,b) (a>b?a:b)
   #define rf24_min(a,b) (a<b?a:b)
 
-	
-//Generic Linux/ARM and //http://iotdk.intel.com/docs/master/mraa/
-#if ( defined (__linux) || defined (LINUX) ) && defined( __arm__ ) || defined(MRAA) // BeagleBone Black running GNU/Linux or any other ARM-based linux device
+  #if defined SPI_HAS_TRANSACTION && !defined SPI_UART && !defined SOFTSPI
+    #define RF24_SPI_TRANSACTIONS
+  #endif
+ 
+ 
+//ATXMega
+#if defined(__AVR_ATxmega64D3__) || defined(__AVR_ATxmega128D3__) || defined(__AVR_ATxmega192D3__) || defined(__AVR_ATxmega256D3__) || defined(__AVR_ATxmega384D3__) // In order to be available both in windows and linux this should take presence here.
+  #define XMEGA
+  #define XMEGA_D3
+  #include "utility/ATXMegaD3/RF24_arch_config.h"
+#elif ( !defined (ARDUINO) ) // Any non-arduino device is handled via configure/Makefile
 
-  // The Makefile checks for bcm2835 (RPi) and copies the correct includes.h file to /arch/includes.h (Default is spidev config)
-  // This behavior can be overridden by calling 'make RF24_SPIDEV=1' or 'make RF24_MRAA=1'
-  // The includes.h file defines either RF24_RPi, MRAA or RF24_BBB and includes the correct RF24_arch_config.h file
-  #include "arch/includes.h"
-
-//Arduino Due
-#elif defined ARDUINO_SAM_DUE 
-  
-  #define RF24_DUE
-  #include "arch/Due/RF24_arch_config.h"
+  // The configure script detects device and copies the correct includes.h file to /utility/includes.h
+  // This behavior can be overridden by calling configure with respective parameters
+  // The includes.h file defines either RF24_RPi, MRAA, LITTLEWIRE or RF24_SPIDEV and includes the correct RF24_arch_config.h file
+  #include "utility/includes.h"
 
 //ATTiny  
-#elif defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__) || defined(__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
-  
+#elif defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__) || defined(__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__) || defined(__AVR_ATtiny2313__) || defined(__AVR_ATtiny4313__) || defined(__AVR_ATtiny861__)  
   #define RF24_TINY
-  #include "arch/ATTiny/RF24_arch_config.h"
+  #include "utility/ATTiny/RF24_arch_config.h"
+
 
 //LittleWire  
 #elif defined(LITTLEWIRE)
   
-  #include "arch/LittleWire/RF24_arch_config.h"
+  #include "utility/LittleWire/RF24_arch_config.h"
 
 //Teensy  
 #elif defined (TEENSYDUINO)
 
-  #include "arch/Teensy/RF24_arch_config.h"  
+  #include "utility/Teensy/RF24_arch_config.h"  
 //Everything else
 #else 
 
-  #if ARDUINO < 100
-	#include <WProgram.h>
-  #else
-	#include <Arduino.h>
-  #endif
-
-  #include <stddef.h>
+  #include <Arduino.h>
   
- 
-  // Define _BV for non-Arduino platforms and for Arduino DUE
+  // RF modules support 10 Mhz SPI bus speed
+  const uint32_t RF24_SPI_SPEED = 10000000;  
+
 #if defined (ARDUINO) && !defined (__arm__) && !defined (__ARDUINO_X86__)
       #if defined SPI_UART
 		#include <SPI_UART.h>
@@ -87,6 +83,7 @@
 		#define _SPI SPI
 	  #endif
 #else
+  // Define _BV for non-Arduino platforms and for Arduino DUE
   #include <stdint.h>
   #include <stdio.h>
   #include <string.h>
@@ -106,7 +103,6 @@
   #define _SPI SPI
 #endif
 
-  
   #ifdef SERIAL_DEBUG
 	#define IF_SERIAL_DEBUG(x) ({x;})
   #else
@@ -121,20 +117,21 @@
 	#define _BV(bit) (1<<(bit))
 #endif
   
-// Avoid spurious warnings
-// Arduino DUE is arm and uses traditional PROGMEM constructs
-#if 1
-#if ! defined( NATIVE ) && defined( ARDUINO ) && ! defined(__arm__)
-#undef PROGMEM
-#define PROGMEM __attribute__(( section(".progmem.data") ))
-#undef PSTR
-#define PSTR(s) (__extension__({static const char __c[] PROGMEM = (s); &__c[0];}))
-#endif
-#endif
-
 // Progmem is Arduino-specific
 // Arduino DUE is arm and does not include avr/pgmspace
-#if defined(ARDUINO) && ! defined(__arm__) && !defined (__ARDUINO_X86__)
+#if defined (ARDUINO_ARCH_ESP8266)
+
+  #define PSTR(x) (x)
+  #define printf Serial.printf
+  #define sprintf(...) os_sprintf( __VA_ARGS__ )
+  #define printf_P printf
+  #define strlen_P strlen  
+  #define PROGMEM
+  #define pgm_read_byte(addr) (*(const unsigned char *)(addr))
+  #define pgm_read_word(p) (*(p))
+  #define PRIPSTR "%s"
+
+#elif defined(ARDUINO) && ! defined(__arm__) && !defined (__ARDUINO_X86__) || defined(XMEGA)
 	#include <avr/pgmspace.h>
 	#define PRIPSTR "%S"
 #else

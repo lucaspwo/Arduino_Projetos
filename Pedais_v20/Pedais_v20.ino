9ba49e73a2f3fcb5d4ca78568d9c19a2b1bf2f72
Lucas Oliveira <lucaspwo@gmail.com>
2019-01-29 15:10:46 -0300

    lib update

---
+++ Pedais_v20/Pedais_v20.ino
@@ -0,0 +1,96 @@
+// Simple gamepad example that demonstraits how to read five Arduino
+// digital pins and map them to the Arduino Joystick library.
+//
+// The digital pins 2 - 6 are grounded when they are pressed.
+// Pin 2 = UP
+// Pin 3 = RIGHT
+// Pin 4 = DOWN
+// Pin 5 = LEFT
+// Pin 6 = FIRE
+//
+// NOTE: This sketch file is for use with Arduino Leonardo and
+//       Arduino Micro only.
+//
+// by Matthew Heironimus
+// 2016-11-24
+//--------------------------------------------------------------------
+
+#include <Joystick.h>
+
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_JOYSTICK,
+  0, 0,                  // Button Count, Hat Switch Count
+  false, false, false,     // X and Y, but no Z Axis
+  false, false, true,   // No Rx, Ry, or Rz
+  false, false,          // No rudder or throttle
+  false, false, false);  // No accelerator, brake, or steering
+
+void setup() {
+  // Initialize Button Pins
+  /*pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);*/
+
+  // Initialize Joystick Library
+  Joystick.begin();
+  Joystick.setRzAxisRange(1023, -1023);
+  //Joystick.setBrakeRange(1023, 0);
+}
+
+// Last state of the buttons
+//int lastButtonState[5] = {0,0,0,0,0};
+int thro, brak;
+
+void loop() {
+
+  // Read pin values
+  thro = analogRead(A0);
+  //Joystick.setThrottle(thro);
+  brak = analogRead(A1);
+  //Joystick.setBrake(brak);
+  Joystick.setRzAxis(thro-brak);
+  /*for (int index = 0; index < 5; index++)
+  {
+    int currentButtonState = !digitalRead(index + 2);
+    if (currentButtonState != lastButtonState[index])
+    {
+      switch (index) {
+        case 0: // UP
+          if (currentButtonState == 1) {
+            Joystick.setYAxis(-1);
+          } else {
+            Joystick.setYAxis(0);
+          }
+          break;
+        case 1: // RIGHT
+          if (currentButtonState == 1) {
+            Joystick.setXAxis(1);
+          } else {
+            Joystick.setXAxis(0);
+          }
+          break;
+        case 2: // DOWN
+          if (currentButtonState == 1) {
+            Joystick.setYAxis(1);
+          } else {
+            Joystick.setYAxis(0);
+          }
+          break;
+        case 3: // LEFT
+          if (currentButtonState == 1) {
+            Joystick.setXAxis(-1);
+          } else {
+            Joystick.setXAxis(0);
+          }
+          break;
+        case 4: // FIRE
+          Joystick.setButton(0, currentButtonState);
+          break;
+      }
+      lastButtonState[index] = currentButtonState;
+    }
+  }*/
+
+  //delay(10);
+}

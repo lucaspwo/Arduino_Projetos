b248cf6c8e187340d799ff9a73c01cbd977a3ddd
Lucas Oliveira <lucas@lucaspwo.com>
2026-03-17 14:39:23 -0300

    Atualização Ancalagon - 17/03/2026

--- libraries/IRremote/examples/MultipleReceivers/MultipleReceivers.ino
+++ libraries/IRremote/examples/MultipleReceivers/MultipleReceivers.ino
@@ -1,15 +1,14 @@
 /*
- * SimpleReceiver.cpp
+ * MultipleReceivers.cpp
  *
- * Demonstrates receiving ONLY NEC protocol IR codes with IRremote
- * If no protocol is defined, all protocols (except Bang&Olufsen) are active.
+ * Demonstrates receiving from multiple receivers at different pins using multiple IRreceiver instances
  *
  *  This file is part of Arduino-IRremote https://github.com/Arduino-IRremote/Arduino-IRremote.
  *
  ************************************************************************************
  * MIT License
  *
- * Copyright (c) 2020-2025 Armin Joachimsmeyer
+ * Copyright (c) 2025 Armin Joachimsmeyer
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -33,6 +32,8 @@
 
 #include <Arduino.h>
 
+#define IR_RECEIVE_PIN_OF_SECOND_RECEIVER   5
+
 /*
  * Specify which protocol(s) should be used for decoding.
  * If no protocol is defined, all protocols (except Bang&Olufsen) are active.
@@ -48,28 +49,28 @@
 //#define DECODE_SONY
 //#define DECODE_RC5
 //#define DECODE_RC6
-
 //#define DECODE_BOSEWAVE
 //#define DECODE_LEGO_PF
 //#define DECODE_MAGIQUEST
 //#define DECODE_WHYNTER
 //#define DECODE_FAST
-
 //#define DECODE_DISTANCE_WIDTH // Universal decoder for pulse distance width protocols
 //#define DECODE_HASH         // special decoder for all protocols
-
 //#define DECODE_BEO          // This protocol must always be enabled manually, i.e. it is NOT enabled if no protocol is defined. It prevents decoding of SONY!
-
 //#define DEBUG               // Activate this for lots of lovely debug output from the decoders.
-
-//#define USE_THRESHOLD_DECODER  // May give slightly better results especially for jittering signals and protocols with short 1 pulses / pauses. Saves 110 bytes program memory.
 //#define RAW_BUFFER_LENGTH  750 // For air condition remotes it may require up to 750. Default is 200.
+#define SUPPORT_MULTIPLE_RECEIVER_INSTANCES
+void UserIRReceiveTimerInterruptHandler(); // must also be before line #include <IRremote.hpp>
 
 /*
  * This include defines the actual pin number for pins like IR_RECEIVE_PIN, IR_SEND_PIN for many different boards and architectures
  */
 #include "PinDefinitionsAndMore.h"
 #include <IRremote.hpp> // include the library
+
+IRrecv MySecondIrReceiver(IR_RECEIVE_PIN_OF_SECOND_RECEIVER); // This sets the pin for the second instance
+
+void handleSuccessfulDecoding(IRrecv *aIRReceiverInstance);
 
 void setup() {
     Serial.begin(115200);
@@ -78,11 +79,11 @@ void setup() {
     Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
 
     // Start the receiver and if not 3. parameter specified, take LED_BUILTIN pin from the internal boards definition as default feedback LED
-    IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+    IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK); // This sets the pin for the default / first instance and enables the global LED feedback
 
     Serial.print(F("Ready to receive IR signals of protocols: "));
     printActiveIRProtocols(&Serial);
-    Serial.println(F("at pin " STR(IR_RECEIVE_PIN)));
+    Serial.println(F("at pin " STR(IR_RECEIVE_PIN) " and pin " STR(IR_RECEIVE_PIN_OF_SECOND_RECEIVER)));
 }
 
 void loop() {
@@ -95,37 +96,53 @@ void loop() {
      * and up to 32 bit raw data in IrReceiver.decodedIRData.decodedRawData
      */
     if (IrReceiver.decode()) {
+        handleSuccessfulDecoding(&IrReceiver);
+    }
+    if (MySecondIrReceiver.decode()) {
+        handleSuccessfulDecoding(&MySecondIrReceiver);
+    }
+}
 
-        /*
-         * Print a summary of received data
-         */
-        if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
-            Serial.println(F("Received noise or an unknown (or not yet enabled) protocol"));
-            // We have an unknown protocol here, print extended info
-            IrReceiver.printIRResultRawFormatted(&Serial, true);
+void handleSuccessfulDecoding(IRrecv *aIRReceiverInstance) {
 
-            IrReceiver.resume(); // Do it here, to preserve raw data for printing with printIRResultRawFormatted()
-        } else {
-            IrReceiver.resume(); // Early enable receiving of the next IR frame
+    Serial.print(F("Receiver at pin "));
+    Serial.print(aIRReceiverInstance->irparams.IRReceivePin);
+    Serial.print(F(": "));
 
-            IrReceiver.printIRResultShort(&Serial);
-            IrReceiver.printIRSendUsage(&Serial);
-        }
-        Serial.println();
+    /*
+     * Print a summary of received data
+     */
+    if (aIRReceiverInstance->decodedIRData.protocol == UNKNOWN) {
+        Serial.println(F("Received noise or an unknown (or not yet enabled) protocol"));
+        // We have an unknown protocol here, print extended info
+        aIRReceiverInstance->printIRResultRawFormatted(&Serial, true);
 
-        /*
-         * Finally, check the received data and perform actions according to the received command
-         */
-        if (IrReceiver.decodedIRData.flags & IRDATA_FLAGS_IS_REPEAT) {
-            Serial.println(F("Repeat received. Here you can repeat the same action as before."));
-        } else {
-            if (IrReceiver.decodedIRData.command == 0x10) {
-                Serial.println(F("Received command 0x10."));
-                // do something
-            } else if (IrReceiver.decodedIRData.command == 0x11) {
-                Serial.println(F("Received command 0x11."));
-                // do something else
-            }
+        aIRReceiverInstance->resume(); // Do it here, to preserve raw data for printing with printIRResultRawFormatted()
+    } else {
+        aIRReceiverInstance->resume(); // Early enable receiving of the next IR frame
+
+        aIRReceiverInstance->printIRResultShort(&Serial);
+        aIRReceiverInstance->printIRSendUsage(&Serial);
+    }
+    Serial.println();
+
+    /*
+     * Finally, check the received data and perform actions according to the received command
+     */
+    if (aIRReceiverInstance->decodedIRData.flags & IRDATA_FLAGS_IS_REPEAT) {
+        Serial.println(F("Repeat received. Here you can repeat the same action as before."));
+    } else {
+        if (aIRReceiverInstance->decodedIRData.command == 0x10) {
+            Serial.println(F("Received command 0x10."));
+            // do something
+        } else if (aIRReceiverInstance->decodedIRData.command == 0x11) {
+            Serial.println(F("Received command 0x11."));
+            // do something else
         }
     }
+
+}
+
+void UserIRReceiveTimerInterruptHandler() {
+    MySecondIrReceiver.ReceiveInterruptHandler();
 }
